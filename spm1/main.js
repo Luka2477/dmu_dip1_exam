3e3ec8673895503231dcb4772451d60ffce61457
luka2477 <luke.da.duke@hotmail.com>
2023-01-02 23:26:36 +0100

    done: question 1

---
+++ spm1/main.js
@@ -0,0 +1,51 @@
+const makes = ["Mercedes Benz", "Audi", "Volkswagen", "Skoda", "Fiat", "Volvo"];
+const wheels = [2, 3, 4, 6, 8, 12, 16];
+
+class Car {
+  constructor(plate, weight, make, wheels) {
+    this.plate = plate;
+    this.weight = weight;
+    this.make = make;
+    this.wheels = wheels;
+  }
+}
+
+const cars = [];
+
+for (let i = 0; i < 100; i++) {
+  const plate = `AB${100 + Math.floor(Math.random() * 899)}CD`;
+  const weight = 1000 + Math.floor(Math.random() * 9000);
+  const make = makes[Math.floor(Math.random() * makes.length)];
+  const currWheels = wheels[Math.floor(Math.random() * wheels.length)];
+
+  const car = new Car(plate, weight, make, currWheels);
+  cars.push(car);
+}
+
+// ----------------------------------------------------------------
+
+const eightWheelers = cars.filter((car) => car.wheels === 8);
+console.log("🚀 ~ file: main.js:28 ~ eightWheelers", eightWheelers);
+
+const plates = cars.map((car) => car.plate);
+console.log("🚀 ~ file: main.js:30 ~ plates", plates);
+
+const lowestWeight = Math.min(...cars.map((car) => car.weight));
+console.log("🚀 ~ file: main.js:32 ~ lowestWeight", lowestWeight);
+
+const mostWheels = cars.filter(
+  (car) => car.wheels === Math.max(...cars.map((car_) => car_.wheels))
+);
+console.log("🚀 ~ file: main.js:36 ~ mostWheels", mostWheels);
+
+const carsWithWheels = {};
+for (let amount of wheels) {
+  carsWithWheels[amount + " wheels"] = cars.filter(
+    (car) => car.wheels === amount
+  ).length;
+}
+console.log("🚀 ~ file: main.js:39 ~ carsWithWheels", carsWithWheels);
+
+// In Javascript, functions can be assigned to variables in the same way that strings or arrays can.
+// They can be passed into other functions as parameters or returned from them as well.
+// A “higher-order function” is a function that accepts functions as parameters and/or returns a function.
